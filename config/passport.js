c8c1ae7e3818bdf498ade66dfa05f8787cb8682b
evy1-ua <evy1@alu.ua.es>
2023-08-10 05:58:17 +0200

    s

--- config/passport.js
+++ config/passport.js
@@ -5,47 +5,39 @@ const User = require('../models/user');
 
 passport.use(new LocalStrategy(
   {
-    // Configuración de la estrategia
+    usernameField: 'email',
+    passwordField: 'password',
   },
   async (email, password, done) => {
     try {
-      console.log("Estrategia de autenticación en proceso...");
-
-      // Lógica de autenticación
-
+      console.log("Estoy en passport.js");
+      // Lógica de autenticación, por ejemplo, buscar el usuario en la base de datos y verificar las credenciales
+      const user = await User.findOneByEmail(email);
       if (!user || !user.comparePassword(password)) {
-        console.log("Autenticación fallida");
         return done(null, false, { message: 'Credenciales inválidas' });
       }
-
-      console.log("Autenticación exitosa");
+      
       return done(null, user);
     } catch (error) {
-      console.error("Error en la estrategia de autenticación:", error);
       return done(error);
     }
   }
 ));
 
 passport.serializeUser((user, done) => {
-  console.log("Serializando usuario:", user.id);
   done(null, user.id);
 });
 
 passport.deserializeUser(async (id, done) => {
   try {
-    console.log("Deserializando usuario:", id);
     const user = await User.findById(id);
-
-    if (!user) {
-      console.log("Usuario no encontrado");
-      return done(null, false);
+    console.log(user);
+    if(!user){
+      return done(null,false);
     }
-
-    console.log("Usuario deserializado:", user);
     done(null, user);
+
   } catch (error) {
-    console.error("Error en la deserialización de usuario:", error);
     done(error);
   }
 });
